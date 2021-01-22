c073934a758d7b921497883f95cdd7801f6e7828
RestrepoTorres <vmanuel.restrepo@udea.edu.co>
2021-01-21 20:15:09 -0500

    voy aformatear

--- js/astar.js
+++ js/astar.js
@@ -1,3 +1,56 @@
-function start(){
-    console.log("loading");
+var canvas;
+var ctx;
+var FPS = 50;
+
+//map
+var col = 50;
+var arrow = 50
+var scenary; //matriz
+
+//tiles
+var widthT;
+var heightT;
+
+
+function start() {
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+
+
+    //calc tile size
+    widthT = parseInt(canvas.width / col);
+    heightT = parseInt(canvas.height / arrow);
+
+    scenary = createArray2D(arrow, col);
+    setInterval(function () {
+        main();
+    }, 1000 / FPS);
+
+
 }
+
+function createArray2D(arrows, columns) {
+    var obj = new Array(arrows);
+    for (i = 0; i < arrows; i++) {
+        obj[i] = new Array(columns)
+    }
+    return obj;
+}
+
+function main() {
+    console.log("paso");
+}
+
+function tile(x, y) {
+    this.x = x;
+    this.y = y;
+
+    //Type (obstaclce = 1; clean = 0)
+    this.type = 0;
+    var random = Math.floor(Math.random() * 5);
+    if (random === 1) {
+        this.type = 1;
+    }
+    //weigth
+}
+
